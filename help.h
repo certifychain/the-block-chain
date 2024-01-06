7f54ea01abaf4a0e7d84164e5e43a2ad86c858ae
sam <sampreethm@proton.me>
2024-01-06 14:16:49 +0530

    meow

---
+++ help.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void please_help_me() {
+    printf("add_block [data] - append a block into the local blockchain\n");
+    printf("example: add_block 10 \n\n");
+    printf("list_blocks - list the local chain blocks\n\n");
+    printf("add_peer - add one node as a peer\n");
+    printf("example: add_peer 172.17.0.10\n\n");
+    printf("list_peers - list the peers\n\n");
+    printf("exit - quit the program\n");
+}
+
