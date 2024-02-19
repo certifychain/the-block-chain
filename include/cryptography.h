99892675578290d606e91b306bafb961961c7bd9
sam <aquaticcalf@proton.me>
2024-02-19 19:19:34 +0530

    meow : the great reset

--- include/cryptography.h
+++
@@ -1,7 +0,0 @@
-#ifndef CERTIFYCHAIN_TBC_CRYPTOGRAPHY
-#define CERTIFYCHAIN_TBC_CRYPTOGRAPHY
-
-#include <openssl/sha.h>
-
-
-#endif
--- include/utils.h
+++
@@ -1,6 +0,0 @@
-#ifndef CERTIFYCHAIN_TBC_UTILS
-#define CERTIFYCHAIN_TBC_UTILS
-
-
-
-#endif
